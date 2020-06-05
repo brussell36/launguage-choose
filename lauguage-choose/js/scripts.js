7306d572229278184635a65246a9c93fec4e5f6b
Ben Russell <benjaminrussell36@gmail.com>
2020-06-05 13:39:21 -0700

    Add span to insert their dropdown menu choice after their name. Add jumbotron div to make my text easier to read.

--- lauguage-choose/js/scripts.js
+++ lauguage-choose/js/scripts.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   $("form#code").submit(function() {
     event.preventDefault();
     const name = $("#name").val();
+    const stack = $("#stack").val();
     const pina = parseInt($('input[name="pina"]:checked').val());
     const rain = parseInt($('input[name="rain"]:checked').val());
     const yoga = parseInt($('input[name="yoga"]:checked').val());
@@ -9,6 +10,7 @@ $(document).ready(function() {
     const result = pina + rain + yoga + champagne;
 
     $(".name").text(name);
+    $(".stack").text(stack);
     
     if (result === 4) {
       $("#js").show();

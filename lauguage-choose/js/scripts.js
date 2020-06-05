9f94e5f05eebe351d0e417cf341a137f2aefad51
Ben Russell <benjaminrussell36@gmail.com>
2020-06-05 09:13:14 -0700

    Add jquery logic to save variables gathered from forms.

--- lauguage-choose/js/scripts.js
+++ lauguage-choose/js/scripts.js
@@ -1,5 +1,13 @@
 $(document).ready(function() {
   $("form#code").submit(function(event) {
     const name = $("#name").val();
+    const stack = $("#stack").val();
+    const pina = parseInt($('input[name=pina]:checked').val());
+    const rain = parseInt($('input[name=rain]:checked').val());
+    const yoga = parseInt($('input[name=yoga]:checked').val());
+    const champagne = parseInt($('input[name=champagne]:checked').val());
+    const result;
+
+    if ()
   });
 });

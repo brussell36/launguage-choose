d0fc2f8e443defaa448b3352809619aac0be4943
Ben Russell <benjaminrussell36@gmail.com>
2020-06-05 09:27:51 -0700

    Add if..else statements to show or hide result variable.

--- lauguage-choose/js/scripts.js
+++ lauguage-choose/js/scripts.js
@@ -6,8 +6,31 @@ $(document).ready(function() {
     const rain = parseInt($('input[name=rain]:checked').val());
     const yoga = parseInt($('input[name=yoga]:checked').val());
     const champagne = parseInt($('input[name=champagne]:checked').val());
-    const result;
+    const result = pina + rain + yoga + champagne;
 
-    if ()
+    if (result <= 4) {
+      $("#js").show();
+      $("#swift").hide();
+      $("#java").hide();
+      $("#scala").hide();
+    }
+    else if (result > 4 || result <= 7) {
+      $("#swift").show();
+      $("#java").hide();
+      $("#scala").hide();
+      $("#js").hide();
+    }
+    else if (result > 7 || result <= 9) {
+      $("#java").show();
+      $("#scala").hide();
+      $("#js").hide();
+      $("#swift").hide();
+    }
+    else {
+      $("#scala").show();
+      $("#js").hide();
+      $("#swift").hide();
+      $("#java").hide();
+    }
   });
 });

0ffe98c2c9e1bc5928a578cb861a7e1b0113436a
Ben Russell <benjaminrussell36@gmail.com>
2020-06-05 10:15:11 -0700

    Add background-image.

--- lauguage-choose/js/scripts.js
+++ lauguage-choose/js/scripts.js
@@ -1,26 +1,25 @@
 $(document).ready(function() {
-  $("form#code").submit(function(event) {
-    const name = $("#name").val();
-    const stack = $("#stack").val();
+  $("form#code").submit(function() {
+    event.preventDefault();
     const pina = parseInt($('input[name=pina]:checked').val());
     const rain = parseInt($('input[name=rain]:checked').val());
     const yoga = parseInt($('input[name=yoga]:checked').val());
     const champagne = parseInt($('input[name=champagne]:checked').val());
     const result = pina + rain + yoga + champagne;
-
-    if (result <= 4) {
+console.log(rain);
+    if (result === 4) {
       $("#js").show();
       $("#swift").hide();
       $("#java").hide();
       $("#scala").hide();
     }
-    else if (result > 4 || result <= 7) {
+    else if (result > 4 && result <= 7) {
       $("#swift").show();
       $("#java").hide();
       $("#scala").hide();
       $("#js").hide();
     }
-    else if (result > 7 || result <= 9) {
+    else if (result > 7 && result <= 9) {
       $("#java").show();
       $("#scala").hide();
       $("#js").hide();
